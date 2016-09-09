e5f4261a45da823d43729165aa3f6d583ea84ed8
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-09-09 19:04:26 -0400

    Add user data/access token request to login callback

--- api.py
+++ api.py
@@ -1,5 +1,14 @@
 from flask import Flask, request
+import urllib
+import urllib2
+import os
 app = Flask(__name__)
+
+API_URL = "api.pictobattle.com"
+
+LOGIN_CALLBACK_URI = "/user/logIn/callback/"
+INSTAGRAM_ID = os.environ['INSTAGRAM_ID']
+INSTAGRAM_SECRET = os.environ['INSTAGRAM_SECRET']
 
 
 @app.route("/")
@@ -7,6 +16,14 @@ def index():
     return "Hello World!"
 
 
-@app.route("/user/logIn/callback/")
+@app.route(LOGIN_CALLBACK_URI)
 def login_callback():
-    return str(request.args.get('code'))
+    GRANT_TYPE = 'authorization_code'
+    url_data = {'client_id': INSTAGRAM_ID, 'client_secret': INSTAGRAM_SECRET,
+                'grant_type': GRANT_TYPE,
+                'redirect_uri': API_URL + LOGIN_CALLBACK_URI,
+                'code': request.args.get('code')}
+    url_data = urllib.urlencode(url_data)
+    user_data = urllib2.urlopen('https://api.instagram.com/oauth/access_token',
+                                url_data).read()
+    return str(user_data)
